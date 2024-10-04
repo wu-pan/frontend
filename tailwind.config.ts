5970e282188d89d66258033c4612871dc45d789f
wupan <wupan_@outlook.com>
2024-10-04 22:53:40 +0800

    feat(core): :tada: 项目初始化配置

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{ts,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
