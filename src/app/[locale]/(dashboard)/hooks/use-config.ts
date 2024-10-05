945d76c2ea3b503be290e0c3f9a8748a0d2a1895
wupan <wupan_@outlook.com>
2024-10-05 15:06:21 +0800

    删除Song模块

--- src/app/[locale]/(dashboard)/hooks/use-config.ts
+++
@@ -1,9 +0,0 @@
-import { useAtom } from "jotai/react";
-import { atomWithStorage } from "jotai/utils";
-
-const configAtom = atomWithStorage("config", {
-  style: "default",
-  theme: "zine",
-});
-
-export const useConfig = () => useAtom(configAtom);
